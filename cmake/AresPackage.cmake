abe44a442aea8d25ddb351ecc42ac55a85ff1c57
Ningyuan Li <mariotaku.lee@gmail.com>
2022-12-26 17:20:35 +0900

    updated release config

--- cmake/AresPackage.cmake
+++ cmake/AresPackage.cmake
@@ -1,1 +1,14 @@
 execute_process(COMMAND ares-package "${CPACK_TEMPORARY_DIRECTORY}" -o "${CPACK_PACKAGE_DIRECTORY}")
+
+find_program(GEN_MANIFEST_CMD webosbrew-gen-manifest)
+if (GEN_MANIFEST_CMD)
+    execute_process(COMMAND "${GEN_MANIFEST_CMD}"
+            -a "${CPACK_TEMPORARY_DIRECTORY}/appinfo.json"
+            -p "${CPACK_PACKAGE_DIRECTORY}/${CPACK_PACKAGE_FILE_NAME}.ipk"
+            -o "${CPACK_PACKAGE_DIRECTORY}/${CPACK_PACKAGE_NAME}.manifest.json"
+            -i "https://github.com/mariotaku/ihsplay/raw/master/deploy/webos/largeIcon.png"
+            -l "https://github.com/mariotaku/ihsplay"
+            )
+else ()
+    message("Skip webOS homebrew manifest generation because command line tool is not found")
+endif ()
--- cmake/PackageWebOS.cmake
+++ cmake/PackageWebOS.cmake
@@ -1,6 +1,3 @@
-install(TARGETS ihsplay RUNTIME DESTINATION .)
-install(DIRECTORY ${CMAKE_SOURCE_DIR}/deploy/webos/ DESTINATION .)
-
 set(CPACK_PACKAGE_NAME "org.mariotaku.ihsplay")
 set(CPACK_GENERATOR "External")
 set(CPACK_EXTERNAL_PACKAGE_SCRIPT "${CMAKE_SOURCE_DIR}/cmake/AresPackage.cmake")
@@ -8,6 +5,13 @@ set(CPACK_EXTERNAL_ENABLE_STAGING TRUE)
 set(CPACK_MONOLITHIC_INSTALL TRUE)
 set(CPACK_PACKAGE_DIRECTORY ${CMAKE_SOURCE_DIR}/dist)
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}_${PROJECT_VERSION}_$ENV{ARCH}")
+
+# Copy manifest
+configure_file(deploy/webos/appinfo.json.in ./appinfo.json @ONLY)
+
+install(TARGETS ihsplay RUNTIME DESTINATION .)
+install(DIRECTORY ${CMAKE_SOURCE_DIR}/deploy/webos/ DESTINATION . PATTERN ".*" EXCLUDE PATTERN "*.in" EXCLUDE)
+install(FILES "${CMAKE_BINARY_DIR}/appinfo.json" DESTINATION .)
 
 # Will use all cores on CMake 3.20+
 set(CPACK_THREADS 0)
