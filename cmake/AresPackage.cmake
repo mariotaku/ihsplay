b8fce2dd5e11753d520fa548c905e893701e7b79
Ningyuan Li <mariotaku.lee@gmail.com>
2023-06-20 19:21:55 +0900

    updated build config

--- cmake/AresPackage.cmake
+++ cmake/AresPackage.cmake
@@ -8,9 +8,7 @@ execute_process(COMMAND ares-package "${CPACK_TEMPORARY_DIRECTORY}" -o "${CPACK_
 
 find_program(GEN_MANIFEST_CMD webosbrew-gen-manifest)
 if (GEN_MANIFEST_CMD)
-    execute_process(COMMAND "${GEN_MANIFEST_CMD}"
-            -a "${CPACK_TEMPORARY_DIRECTORY}/appinfo.json"
-            -p "${CPACK_PACKAGE_DIRECTORY}/${CPACK_PACKAGE_FILE_NAME}.ipk"
+    execute_process(COMMAND "${GEN_MANIFEST_CMD}" -p "${CPACK_PACKAGE_DIRECTORY}/${CPACK_PACKAGE_FILE_NAME}.ipk"
             -o "${CPACK_PACKAGE_DIRECTORY}/${CPACK_PACKAGE_NAME}.manifest.json"
             -i "https://github.com/mariotaku/ihsplay/raw/master/deploy/webos/largeIcon.png"
             -l "https://github.com/mariotaku/ihsplay"
